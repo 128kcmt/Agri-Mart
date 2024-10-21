db2bc9bc17633d87d3cab01e0a7db203f5525b0e
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-10-21 14:09:23 +0200

    installed typeorm with postgres and setup the config file

---
+++ typeOrmConfig.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+
+export const typeOrmConfig : TypeOrmModuleOptions = {
+    type : 'postgres',
+    host: '',
+    port: 5432,
+    username: '',
+    password:'',
+    database:'',
+    entities:[],
+    synchronize: false
+}
